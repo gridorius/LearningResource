ebfb6214f1d41aefebb555b4e1a8962cd5fe2f6d
Avokado <alotofcode95@gmail.com>
2018-08-15 21:28:05 +0600

    Добавьте файлы проекта.

---
+++ patternsLearning/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace patternsLearning
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            /*
+            bundles.Add(new ScriptBundle("~/bundles/name").Include(
+                        "~/Scripts/name.js"));
+
+            bundles.Add(new StyleBundle("~/Content/name").Include(
+                      "~/Content/name.css");
+            */
+        }
+    }
+}
