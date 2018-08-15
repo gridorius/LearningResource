ce0b91521899fae8c33c5b548b52ce1d57a55705
gridorius <gridorius@yandex.ru>
2018-08-15 18:47:52 +0500

    Add content

--- patternsLearning/Controllers/HomeController.cs
+++ patternsLearning/Controllers/HomeController.cs
@@ -3,28 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using patternsLearning.Models;
+using Newtonsoft.Json.Linq;
 
 namespace patternsLearning.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller 
     {
+
+        //sec_description - эт и есть краткая информация, шоб тебя
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+        public ActionResult BriefDescription() {
+            siteDbEntities db;
+            try
+            {
+                db = new siteDbEntities();
+                JObject description = JObject.FromObject(
+                    new
+                    {
+                        art =
+                    from s in db.section
+                    select new
+                    {
+                        sec_name = s.sec_name,
+                        sec_description = s.sec_description,
+                        sec_front_pic = s.sec_front_pic,
+                        sec_back_pick = s.sec_back_pic
+                    }
+                    }
+                        );
+                ViewBag.descJSON = description;
+                return PartialView();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return PartialView();
+            }
         }
     }
 }
---
+++ patternsLearning/PageStructure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using patternsLearning.Models;
+using System.Web.Mvc;
+
+namespace patternsLearning
+{
+    public static class PageStructure
+    {
+        public static JObject getSection(siteDbEntities db, string secName ) {
+            db = new siteDbEntities();
+            var section = db.section.Where(s => s.sec_name == secName).Single();
+            JObject sectionJSON = JObject.FromObject(new
+            {
+                section = new
+                {
+                    sec_name = section.sec_name,
+                    sec_description = section.sec_description,
+                    category = from c in db.category
+                               where c.sec_id == section.sec_id
+                               select new
+                               {
+                                   cat_name = c.cat_name,
+                                   cat_description = c.cat_description,
+                                   article = from a in db.article
+                                             where a.art_id == c.sec_id
+                                             select new
+                                             {
+                                                 art_name = a.art_name,
+                                                 art_description = a.art_description
+                                             }
+                               }
+                }
+            });
+            return sectionJSON;
+        }
+    }
+}
