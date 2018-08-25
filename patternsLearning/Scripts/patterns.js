97b38663540ae0e832735cd80593f15ee2ab271f
gridorius <gridorius@yandex.ru>
2018-08-26 00:06:09 +0500

    ебаный рот этого жс

--- patternsLearning/Scripts/patterns.js
+++ patternsLearning/Scripts/patterns.js
@@ -3,7 +3,7 @@
     <div>
             <h2 @click='selected = !selected;$root.showInfo = false' :title='description'>{{name}}</h2>
             <ul :class='{open:selected}'>
-                <li v-for='art in article'><div :title='art.art_description'>{{art.art_name}}</div></li>
+                <li v-for='art in article'><div :title='art.art_description' @click='$root.showArticle(art.art_name)'>{{art.art_name}}</div></li>
             </ul>
         </div>
     `,
@@ -20,15 +20,21 @@ let app = new Vue({
     data: {
         showInfo: true,
         categories: [],
-        info:''
+        info: '',
+        article: false,
     },
     methods: {
         setCategories(obj) {
             this.info = obj.section.sec_description;
             let categories = obj.section.category;
             this.categories.push(...categories.splice(0, 3));
+        },
+        showArticle(name) {
+            let data = new FormData();
+            data.append('artName', name);
+            fetch('/Patterns/Articles', { method: 'post', body: data }).then(r => r.text()).then(t => JSON.parse(t.replace(/&quot;/g, '\"'))).then(o => app.article = o.article[0]);
         }
     }
 });
 
-fetch('/Patterns/PatternPageStructure').then(r => r.text()).then(t => JSON.parse(t.replace(/&quot;/g, '\"'))).then(app.setCategories);
+fetch('/Patterns/PatternPageStructure').then(r => r.text()).then(t => JSON.parse(t)).then(app.setCategories);
