1883d46881e33b7b15a7dde455db26b14cd74226
Avokado <alotofcode95@gmail.com>
2018-08-28 23:25:35 +0600

    I do it :(

---
+++ patternsLearning/Controllers/AdminPanelController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace patternsLearning.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        // GET: AdminPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //ну тут крч всякая дичь пошла
+        [Authorize]
+        public RedirectToRouteResult AddSection() {
+            return  RedirectToAction("Index","AdminPanel");
+        }
+    }
+}
---
+++ patternsLearning/Controllers/AssignController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using patternsLearning.Models;
+using System.Web.Security;
+
+namespace patternsLearning.Controllers
+{
+    public class AssignController : Controller
+    {
+        // GET: Assign
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Assign
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid) {
+                User user = null;
+                using (UserEntities db = new UserEntities()) {
+                    user = db.User.FirstOrDefault(u => u.Login == model.Login && u.Password==model.Password);
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Login, true);
+                    return RedirectToAction("Index","AdminPanel");
+                }
+                else {
+                    ModelState.AddModelError("", "Ошибка авторизации! Пользователя с такими логином и паролем не существует");
+                }
+            }
+            return View();
+        }
+    }
+}
