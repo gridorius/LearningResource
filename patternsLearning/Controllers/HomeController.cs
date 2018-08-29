f855f67b91b1de322860124abd076c96be8d4118
Avokado <alotofcode95@gmail.com>
2018-08-29 21:11:16 +0600

    add controller cell

--- patternsLearning/Controllers/HomeController.cs
+++ patternsLearning/Controllers/HomeController.cs
@@ -31,7 +31,8 @@ namespace patternsLearning.Controllers
                             sec_name = s.sec_name,
                             sec_description = s.sec_description,
                             sec_front_pic = s.sec_front_pic,
-                            sec_back_pic = s.sec_back_pic
+                            sec_back_pic = s.sec_back_pic,
+                            sec_controller = s.sec_controller
                         }
                         }
                             );
