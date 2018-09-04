6625e9aa4b264c935bfe50b832604cb87e1cd068
gridorius <gridorius@yandex.ru>
2018-09-04 06:00:59 +0500

    id motherfucker

--- patternsLearning/Scripts/patterns.js
+++ patternsLearning/Scripts/patterns.js
@@ -3,7 +3,7 @@
     <div>
             <h2 class='catergory' @click='selected = !selected;$root.showInfo = false' :title='description'>{{name}}</h2>
             <ul :class='{open:selected}'>
-                <li class='pattern' v-for='art in article'><div :title='art.art_description' @click='$root.showArticle(art.art_name)'>{{art.art_name}}</div></li>
+                <li class='pattern' v-for='art in article'><div :title='art.art_description' @click='$root.showArticle(art.art_id)'>{{art.art_name}}</div></li>
             </ul>
         </div>
     `,
@@ -29,12 +29,12 @@ let app = new Vue({
             let categories = obj.section.category;
             this.categories.push(...categories.splice(0, 3));
         },
-        showArticle(name) {
+        showArticle(id) {
             let data = new FormData();
-            data.append('artName', name);
-            fetch('/Patterns/Articles', { method: 'post', body: data }).then(r => r.text()).then(t => JSON.parse(t)).then(o => app.article = o.article[0]);
+            data.append('artId', id);
+            fetch('/Patterns/Articles', { method: 'post', body: data }).then(r => r.json()).then(o => this.article = o.article[0]);
         }
     }
 });
 
-fetch('/Patterns/PatternPageStructure').then(r => r.text()).then(t => JSON.parse(t)).then(app.setCategories);
+fetch('/Patterns/PatternPageStructure').then(r => r.json()).then(app.setCategories);
